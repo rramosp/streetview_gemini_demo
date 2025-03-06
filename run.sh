01b2565642b618745477629bb819c2d1079c23a2
raulramos <raulramos@google.com>
2025-03-06 15:47:36 -0500

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
+if [ -z "${GEMINI_API_KEY}" ]; then
+    echo "error: must set GEMINI_API_KEY env var"
+    exit -1
+fi
+
+if [ -z "${STREETVIEW_API_KEY}" ]; then
+    echo "error: must set STREETVIEW_API_KEY env var"
+    exit -1
+fi
+
+
 sudo docker run -p 8501:8501  \
                 -e DISPLAY=$DISPLAY  \
                 -e GEMINI_API_KEY=$GEMINI_API_KEY \
